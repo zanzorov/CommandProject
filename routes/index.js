86e3b3d5eafa687934c8ba880123975bde41a999
viskhan <viskhan.rkb@gmail.com>
2022-06-03 14:06:38 +0300

    дополнил контроллы, модели, роуты, изменил index

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ const { Router } = require("express");
 
 const route = Router();
 
-
+route.use(require('./brands.route'));
+route.use(require('./carts.route'));
+route.use(require('./categories.route'));
+route.use(require('./clients.route'));
+route.use(require('./products.route'));
+route.use(require('./reviews.route'));
 
 module.exports = route;
